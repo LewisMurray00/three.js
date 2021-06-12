d32fb3aea75861fd3eacf27192cedd3d6cee6ab9
Lewis Murray <lewisdmurray00@gmail.com>
2021-06-12 21:09:33 +0100

    Added some vertices and geometry

--- Drawing-lines/main.js
+++ Drawing-lines/main.js
@@ -7,3 +7,14 @@ camera.position.set(0,0,100);
 camera.lookAt(0,0,0);
 
 const scene = new THREE.Scene();
+
+//Define a material, for lines LineBasicMaterial/LineDashedMaterial are used
+const material = new THREE.LineBasicMaterial({color: 0x0000ff});
+
+//Adding a geometry with some vertices
+const points = [];
+points.push(new THREE.Vector3(10,0,0));
+points.push(new THREE.Vector3(0,10,0));
+points.push(new THREE.Vector3(10,0,0));
+
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
